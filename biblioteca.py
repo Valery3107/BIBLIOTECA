aa2f603a557f863393b223bba990f0063a7b8a3f
Valery3107 <vjcastro@ucundinamarca.edu.co>
2025-04-09 14:55:42 -0500

    segmentacion 1

---
+++ biblioteca.py
@@ -0,0 +1,7 @@
+from herencia import Biblioteca
+
+if __name__ == "__main__":
+    biblioteca = Biblioteca()
+    print("¡Bienvenido al Sistema de Gestión de Biblioteca!")
+    biblioteca.menu_principal()
+    
